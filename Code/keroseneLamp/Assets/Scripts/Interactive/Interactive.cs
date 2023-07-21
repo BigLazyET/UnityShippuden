2daa1a8b451e2086117eae77b733706df2502fca
xjl32931 <xjl32931@ly.com>
2023-07-21 13:17:18 +0800

    ddd

--- Code/keroseneLamp/Assets/Scripts/Interactive/Interactive.cs
+++ Code/keroseneLamp/Assets/Scripts/Interactive/Interactive.cs
@@ -8,12 +8,12 @@ namespace Assets.Scripts.Interactive
         public GameObject[] triggers;   // 互动触发能与之接触的物体集合
 
         // 针对普通互动和特殊互动
-        private bool isTouch;   // 两者是否在互动触发的接触范围内
+        protected bool isTouch;   // 两者是否在互动触发的接触范围内
         public bool isNeedTouch;   // 互动触发是否需要接触
-        private bool isInput;   // 互动触发的前提：按键是否按下且正确按键
+        protected bool isInput;   // 互动触发的前提：按键是否按下且正确按键
         public bool isNeedInput;   // 互动触发是否需要按键输入
 
-        // 针对特殊互动
+        // 针对特殊互动：此情况比较特殊，可能后期获得了某个装备/到达了某个地方/解锁了某个技能都能触发特殊互动
         private bool isSpecialCheck;  // 特殊互动的先决条件
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -25,27 +25,5 @@ namespace Assets.Scripts.Interactive
         public abstract void NormalActive();
 
         public abstract void SpecificActive();
-
-        public virtual bool CanNormalActive()
-        {
-            return true;
-        }
-
-        public virtual bool CanSpecificActive()
-        {
-            return true;
-        }
-
-        public virtual void DoInterActive()
-        {
-            if (CanNormalActive())
-            {
-                NormalActive();
-            }
-            if (CanSpecificActive())
-            {
-                SpecificActive();
-            }
-        }
     }
 }
--- Code/keroseneLamp/Assets/Scripts/Interactive/NpcInteractiveBase.cs
+++ Code/keroseneLamp/Assets/Scripts/Interactive/NpcInteractiveBase.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static InputActions;
 
 namespace Assets.Scripts.Interactive
 {
     // 这边可以继承针对NPC的Input Actions集合
     // 毕竟交互一般是共通的操作，比如对话，只需要按键左右即可等等
-    public class NpcInteractiveBase : Interactive, 
+    public class NpcInteractiveBase : Interactive, INpcInteractiveActions
     {
+        public void OnDialogue(InputAction.CallbackContext context)
+        {
+            isInput = context.started;
+        }
+
         public override void NormalActive()
         {
             Debug.Log("NormalActive");
