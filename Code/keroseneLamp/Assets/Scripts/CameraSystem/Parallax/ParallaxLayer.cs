1f3802708c5af04d24217c519b9045593448a1a9
xjl32931 <xjl32931@ly.com>
2023-08-30 19:01:08 +0800

    ddd

---
+++ Code/keroseneLamp/Assets/Scripts/CameraSystem/Parallax/ParallaxLayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CameraSystem
+{
+    [ExecuteInEditMode]
+    public class ParallaxLayer : MonoBehaviour
+    {
+        public float parallaxFactor;
+
+        public void Move(float delta)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.x -= delta * parallaxFactor;
+
+            transform.localPosition = newPos;
+        }
+
+    }
+}
